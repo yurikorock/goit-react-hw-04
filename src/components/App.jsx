6b574de86d6a6997583122841d98eff0c2c49a1a
Yura Shaplavsky <yurikorock777@gmail.com>
2025-04-28 19:21:31 +0300

    start task 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,37 +4,10 @@ import { useState, useEffect } from "react";
 import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import ContactForm from "./ContactForm/ContactForm";
-import userContacts from "./contacts.json";
-import ContactList from "./ContactList/ContactList";
-import SearchBox from "./SearchBox/Searchbox";
+
 
 const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    const savedContacts = localStorage.getItem("contacts");
-    return savedContacts ? JSON.parse(savedContacts) : userContacts;
-  });
-  const [search, setSearch] = useState("");
-
-  const filterContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(search.toLowerCase())
-  );
-
-  const addContact = (newContact) => {
-    setContacts((prevContacts) => {
-      return [...prevContacts, newContact];
-    });
-  };
-
-  const deleteContact = (contactId) => {
-    setContacts((prevContacts) => {
-      return prevContacts.filter((cont) => cont.id !== contactId);
-    });
-  };
-
-  useEffect(() => {
-    localStorage.setItem("contacts", JSON.stringify(contacts));
-  }, [contacts]);
+  
 
   return (
     <div>
