f7ad64cc957f7888bb7bb3f5a5e0c668a44acc12
Yura Shaplavsky <yurikorock777@gmail.com>
2025-04-29 21:55:05 +0300

    add style to loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,10 @@
-import { ClipLoader } from "react-spinners";
+import { FadeLoader } from "react-spinners";
 import css from "./Loader.module.css";
 
 const Loader = () => {
   return (
-    <div>
-      <ClipLoader color={"#4facfe"} size={50} />
+    <div className={css.loader}>
+      <FadeLoader color="#1695f1" height={15} radius={2} width={5} />
     </div>
   );
 };
