22da1cd0f981461b07a64bfa6435ea1072d3c9f6
Yura Shaplavsky <yurikorock777@gmail.com>
2025-04-29 17:50:47 +0300

    add verify with react toast

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,21 @@ import { useState, useEffect } from "react";
 import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import toast, { Toaster } from "react-hot-toast";
 import SearchBar from "./SearchBar/SearchBar";
 
 const App = () => {
   const [query, setQuery] = useState();
 
   const handleSearch = (newSearch) => {
-    console.log(newSearch);
+    // console.log(newSearch);
   };
 
   return (
     <div>
       <h1 className="title">Picture Search</h1>
       <SearchBar onSubmit={handleSearch} />
+      <Toaster />
     </div>
   );
 };
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+
 export default function SearchBar({ onSubmit }) {
+  const [query, setQuery] = useState("");
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
 
     const search = formData.get("search");
+    if (search.trim() === "") {
+      toast.error("Необхідно ввести текст для пошуку зображень!", {
+        duration: 1500,
+        position: "top-right",
+      });
+      return;
+    }
 
     event.currentTarget.reset();
 
     onSubmit(search);
+    setQuery("");
   };
 
   return (
