f86877db52696a10e90de93b72ed471b02c247ea
Yura <yurikorock777@gmail.com>
2025-05-01 12:34:08 +0300

    edit App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,27 @@
-//App.jsx
-
 import { useState, useEffect, useRef } from "react";
-import reactLogo from "../assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import toast, { Toaster } from "react-hot-toast";
+
 import SearchBar from "./SearchBar/SearchBar";
 import ImageGallery from "./ImageGallery/ImageGallery";
 import { fetchImagesByQuery } from "./helpers/unsplashApi.js";
 import Loader from "./Loader/Loader";
 import ErrorMessage from "./ErrorMessage/ErrorMessage";
 import LoadMoreBtn from "./LoadMoreBtn/LoadMoreBtn.jsx";
-import Modal from "react-modal";
 import ImageModal from "./ImageModal/ImageModal";
-Modal.setAppElement("#root");
 
 const App = () => {
-  const [query, setQuery] = useState(""); //стан запиту в інпуті
-  const [images, setImages] = useState([]); // стан галереї
-  const [error, setError] = useState(false); // стан HTTP запиту
-  const [isLoading, setIsLoading] = useState(false); //стан лоадера
-  const [errorMessage, setErrorMessage] = useState(""); // стан сповіщення про помилку завантаження зображень
-  const [page, setPage] = useState(1); // стан поточної сторінки зображень
+  const [query, setQuery] = useState("");
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [page, setPage] = useState(1);
   const loadMoreBtnRef = useRef(null);
   const [hasMoreImage, setHasMoreImage] = useState(true);
-  const [isModalOpen, setModalOpen] = useState(false); //стан модального вікна
-  const [selectedImage, setSelectedImage] = useState(null); //стан вибраного зображення
+
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   const handleSearch = (newSearch) => {
     setQuery(newSearch);
@@ -34,16 +30,15 @@ const App = () => {
     setHasMoreImage(true);
   };
 
-  const handleLoadMore = async () => {
+  const handleLoadMore = () => {
     setPage((prevPage) => prevPage + 1);
   };
 
   const openModal = (imageUrl) => {
-    if (imageUrl !== selectedImage && !isModalOpen) {
-      setSelectedImage(imageUrl);
-      setModalOpen(true);
-    }
+    setSelectedImage(imageUrl);
+    setModalOpen(true);
   };
+
   const closeModal = () => {
     setModalOpen(false);
     setSelectedImage(null);
@@ -59,14 +54,14 @@ const App = () => {
   }, [images]);
 
   useEffect(() => {
-    if (!query) {
-      return;
-    }
+    if (!query) return;
+
     const fetchImages = async () => {
       try {
         setError(false);
         setIsLoading(true);
         const data = await fetchImagesByQuery(query, page);
+
         if (data.length === 0) {
           setHasMoreImage(false);
           if (page === 1) {
@@ -74,6 +69,7 @@ const App = () => {
           }
           return;
         }
+
         setImages((prevImages) => [...prevImages, ...data]);
       } catch (error) {
         setError(true);
@@ -94,21 +90,21 @@ const App = () => {
       <SearchBar onSubmit={handleSearch} />
       <Toaster />
       {error && <ErrorMessage message={errorMessage} />}
-      {isLoading && <Loader />}
       {!error && !isLoading && (
         <ImageGallery images={images} onImageClick={openModal} />
       )}
+      {isLoading && <Loader />}
       {images.length > 0 && !isLoading && hasMoreImage && (
         <LoadMoreBtn onClick={handleLoadMore} ref={loadMoreBtnRef} />
       )}
-      {isModalOpen && (
-        <ImageModal
-          isOpen={isModalOpen}
-          onClose={closeModal}
-          imageUrl={selectedImage}
-        />
-      )}
+
+      <ImageModal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        imageUrl={selectedImage}
+      />
     </div>
   );
 };
+
 export default App;
--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -2,7 +2,7 @@ import css from "./ImageCard.module.css";
 
 export default function ImageCard({ image, onImageClick }) {
   return (
-    <div className={css.card} onClick={() => onImageClick(image.urls.full)}>
+    <div className={css.card} onClick={() => onImageClick(image.urls.regular)}>
       <img
         src={image.urls.small}
         alt={image.alt_description}
